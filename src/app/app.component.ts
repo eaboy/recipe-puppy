f472a92b340b5f15b4235420a116794ee46f63cb
Eduardo Aboy <eduaboy@gmail.com>
2020-07-06 07:42:17 +0200

    Implemented search in appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Recipe, SearchParams } from './interfaces/recipe.interfaces';
+import { CommunicationService } from './services/communication.service';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,28 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class AppComponent {
 
   searching = false;
+  recipes: Recipe[];
 
   searchForm = new FormGroup({
     course: new FormControl(),
     ingredients: new FormControl()
   });
 
-  doSearch(): void { }
+  constructor(private communicationService: CommunicationService) { }
+
+  doSearch(): void {
+    this.searching = true;
+    this.recipes = [];
+    const searchParams: SearchParams = {
+      course: this.searchForm.value.course,
+      ingredients: this.searchForm.value.ingredients?.split(',').map((ingredient: string) => ingredient.trim()),
+      page: 1
+    };
+    this.communicationService.search(searchParams).subscribe((recipes: Recipe[]) => {
+      console.log(recipes);
+      this.searching = false;
+      this.recipes = this.recipes.concat(recipes);
+    });
+  }
 
 }
