143842d83c836d54fb2190f61815cd987b0236f0
Eduardo Aboy <eduaboy@gmail.com>
2020-07-06 07:54:28 +0200

    Implement intersection observer for infinite scroll on load more component

--- src/app/components/load-more/load-more.component.ts
+++ src/app/components/load-more/load-more.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Output, EventEmitter, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-load-more',
   templateUrl: './load-more.component.html',
   styleUrls: ['./load-more.component.scss']
 })
-export class LoadMoreComponent implements OnInit {
+export class LoadMoreComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private observer: IntersectionObserver;
+
+  @Input() margin = 100;
+  @Output() loadMore = new EventEmitter();
+
+  constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
+    const options: IntersectionObserverInit = {
+      rootMargin: this.margin + 'px'
+    };
+    this.observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry: IntersectionObserverEntry) => {
+        if (entry.isIntersecting) {
+          this.loadMore.emit();
+        }
+      });
+    }, options);
+    this.observer.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.observer.unobserve(this.el.nativeElement);
   }
 
 }
