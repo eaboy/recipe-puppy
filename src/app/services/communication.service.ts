d00be3cedbad6c21da32c2470533b3db33abd2cb
Eduardo Aboy <eduaboy@gmail.com>
2020-07-06 07:32:31 +0200

    Added search method to communication service

--- src/app/services/communication.service.ts
+++ src/app/services/communication.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SearchParams, ApiResponse, OriginalRecipe } from '../interfaces/recipe.interfaces';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommunicationService {
 
-  constructor() { }
+  private readonly urlEndpoint = 'http://www.recipepuppy.com/api/';
+
+  constructor(private http: HttpClient) { }
+
+  search(searchParams: SearchParams): Observable<any> {
+    let params = new HttpParams().set('p', searchParams.page.toString());
+    if (searchParams.course) {
+      params = params.append('q', searchParams.course);
+    }
+    if (searchParams.ingredients) {
+      const ingredients: string = searchParams.ingredients.join(',');
+      params = params.append('i', ingredients);
+    }
+    return this.http.get(this.urlEndpoint, { params }).pipe(
+      map((response: ApiResponse) => response.results
+        .map((recipe: OriginalRecipe) => {
+          return { ...recipe, ingredients: recipe.ingredients.split(', ') };
+        })));
+  }
+
 }
