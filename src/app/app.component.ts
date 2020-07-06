ce2b9aca73e96600b268442acfc7e3d34d6eaf17
Eduardo Aboy <eduaboy@gmail.com>
2020-07-06 07:16:42 +0200

    Added search form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'recipe-puppy';
+
+  searchForm = new FormGroup({
+    course: new FormControl(),
+    ingredients: new FormControl()
+  });
+
+  doSearch(): void { }
+
 }
